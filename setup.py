1d3d408b75233f0f72ddadaf1136af84bfbf3c9d
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-26 16:30:29 +0900

    Update setup.py to add pypi description

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-ani-toolbutton',
@@ -9,6 +16,8 @@ setup(
     packages=find_packages(),
     description='PyQt QToolButton for animation',
     url='https://github.com/yjg30737/pyqt-ani-toolbutton.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-ani-abstractbutton>=0.0.1'
